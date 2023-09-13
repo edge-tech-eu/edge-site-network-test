9e1af8a8c9355230e8db389c3a951844cc4d0507
Frank Boddeke <frank@edgetech.eu>
2023-09-13 18:53:43 +0200

    eth link timeout 5s

--- src/Connectivity.h
+++ src/Connectivity.h
@@ -2,8 +2,8 @@
 #define CONNECTIVITY_H
 
 #define TIMEOUT_WIFI_CONNECT         60000
-#define TIMEOUT_ETHERNET_CONNECT     60000
-#define TIMEOUT_CLOUD_CONNECT       300000
+#define TIMEOUT_ETHERNET_CONNECT      5000
+#define TIMEOUT_CLOUD_CONNECT       180000
 
     bool connectivity_connect(void);
     void connectivity_init(void);
