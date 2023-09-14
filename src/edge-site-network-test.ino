b6b574fea0a0f5a4421c9ace52e3af3c69386424
Frank Boddeke <frank@edgetech.eu>
2023-09-14 10:21:21 +0200

    different boards select and better logging

--- src/edge-site-network-test.ino
+++ src/edge-site-network-test.ino
@@ -2,9 +2,15 @@
 #include "Connectivity.h"
 #include "Led.h"
 #include "ResetReason.h"
-#include "SmartMeter.h"
 #include "RemoteLogger.h"
+#include "Util.h"
 
+#define ETHERNET_BOARD_PARTICLE 0
+#define ETHERNET_BOARD_EDGE     1
+#define ETHERNET_BOARD          ETHERNET_BOARD_PARTICLE
+
+
+STARTUP(System.enableFeature(FEATURE_RESET_INFO));
 STARTUP(BLE.selectAntenna(BleAntennaType::EXTERNAL));
 
 // does not startup connectivity only after Particle.connect() is called
@@ -18,32 +24,33 @@ STARTUP(System.enableFeature(FEATURE_DISABLE_LISTENING_MODE));
 
 
 // serial logger on serial2
+#if ETHERNET_BOARD == ETHERNET_BOARD_PARTICLE
+Serial1LogHandler logHandler(115200, LOG_LEVEL_ALL);
+#endif
+#if ETHERNET_BOARD == ETHERNET_BOARD_EDGE
 Serial2LogHandler logHandler(115200, LOG_LEVEL_ALL);
+#endif
 RemoteLogHandler remoteLogHandler("monitor.edgetech.nl", 8888, "network-test");
 
 // LEDStatus led_debug(RGB_COLOR_RED, LED_PATTERN_SOLID, LED_SPEED_SLOW, LED_PRIORITY_IMPORTANT);
 
 
-unsigned long next_time;
+unsigned long last_time;
+int print_state;
 
 void setup() {
 
   Watchdog.init(WatchdogConfiguration().timeout(60s));
   Watchdog.start();
 
-  // just to test:
+  // just to test ethernet: no credentials is not use wifi
   WiFi.clearCredentials();
-  // WiFi.setCredentials("qq","***REMOVED***");
-  // WiFi.setCredentials("H369A59AFB1","79DDEE7963A3");
-  // WiFi.setCredentials("external-edge-fs","123EVbest!");
-  // p2Site0001 0a10aced202194944a004b38
-  // walter     0a10aced202194944a004cbc
-  // filter:    0a10aced202194944a006158
-  // elrad001:  0a10aced202194944a004be0
 
   led_init();
 
   reset_reason_log();
+
+  Log.info("%s-%s", completeVersion, System.version().c_str());
 
   // publish communication information to the particle cloude every 30s
   Particle.publishVitals(30); 
@@ -57,12 +64,20 @@ void setup() {
     Log.info("Enabling Ethernet...");
     System.enableFeature(FEATURE_ETHERNET_DETECTION);
 
+
     if_wiznet_pin_remap remap = {};
     remap.base.type = IF_WIZNET_DRIVER_SPECIFIC_PIN_REMAP;
 
+#if ETHERNET_BOARD == ETHERNET_BOARD_EDGE
     remap.cs_pin = D1;
     remap.reset_pin = D3;
     remap.int_pin = D0;
+#endif
+#if ETHERNET_BOARD == ETHERNET_BOARD_PARTICLE
+    remap.cs_pin = PIN_INVALID;
+    remap.reset_pin = PIN_INVALID;
+    remap.int_pin = PIN_INVALID;
+#endif
 
     auto ret = if_request(nullptr, IF_REQ_DRIVER_SPECIFIC, &remap, sizeof(remap), nullptr);
 
@@ -79,45 +94,51 @@ void setup() {
 
   connectivity_init();
 
-  smart_meter_init();
+  print_state = -1;
 
-  smart_meter_identify();
-
-  if(!smart_meter_is_usable()) {
-
-    Log.error("No meter!");
-  }
-
-  next_time = millis() + 1000;
+  last_time = millis();
 }
 
 
 void loop() {
 
-  unsigned long now = millis();
-
   Watchdog.refresh();
-
-  if(smart_meter_is_usable()) {
-    smart_meter_process_serial();
-  }
 
   connectivity_connect();
 
-  if(next_time < now) {
+  if((millis()-last_time) >  60000L) {
 
-    while(next_time < now) {
-      next_time += 1000;
-    }
+    Log.info("still here...");
 
-    if(Particle.connected()) {
-        next_time += 30000 - 1000;
-    }
-    
+    last_time = millis();
+  }
+
+
+  bool ethReady = Ethernet.ready();
+  bool wifiReady = WiFi.ready();
+  bool cloudConnected = Particle.connected();
+  int connectingState = connectivity_get_state();
+
+  int new_print_state = connectingState+(ethReady?32:0)+(wifiReady?64:0)+(cloudConnected?128:0);
+
+  if(new_print_state != print_state) {
+
+      last_time = millis();
+
     Log.info("State: %s: Ethernet: %s, WiFi: %s, Cloud: %s",
-      connectivity_state_name(connectivity_get_state()).c_str(),
-      (Ethernet.ready()?"ready":"not ready"),
-      (WiFi.ready()?"ready":"not ready"),
-      (Particle.connected()?"connected":"not connected"));
+      connectivity_state_name(connectingState).c_str(),
+      (ethReady?"ready":"not ready"),
+      (wifiReady?"ready":"not ready"),
+      (cloudConnected?"connected":"not connected"));
+
+    print_state = new_print_state;
+
+    // testing: once connected restart and do again
+    if(cloudConnected) {
+
+      Log.info("Connected, restart"); 
+      delay(1000);
+      System.reset();
+    }
   }
 }
